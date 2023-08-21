1ec9540f997c28caa15ad24a782a9ed4caa1245a
TedMcbur <142521760+Tedmcbur@users.noreply.github.com>
2023-08-21 10:39:53 -0400

    Update RBXloadermod1.lua

--- RBXloadermod1.lua
+++ RBXloadermod1.lua
@@ -2,3 +2,4 @@ Local owner = Ted
 rconsoletitle("RBXloader mod 1 by owner")
 rconsoleprint("No stuff here :3")
 rconsoleprint("Im learning how to code so if there are issues please tell me and list the code and i will try and fix it")
+rconsoleprint("By oath, I will try and not skid. But if i do, im considered a dumbass, if someone else does it, It will reflect")
