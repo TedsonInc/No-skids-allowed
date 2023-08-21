8167111df0ddea06b967a8b194e6d4f5770c678d
TedMcbur <142521760+Tedmcbur@users.noreply.github.com>
2023-08-21 10:27:26 -0400

    Update and rename essay on why skids are garbage to RBXloadermod1.lua

---
+++ RBXloadermod1.lua
@@ -0,0 +1,4 @@
+Local owner = Ted
+rconsoletitle("RBXloader mod 1 by owner")
+rconsoleprint("No stuff here :3")
+rconsoleprint("Im learning how to code so if there are issues please tell me and list the code and i will try and fix it")
